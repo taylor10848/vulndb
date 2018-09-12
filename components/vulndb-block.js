1732e50f22f0f62412cae8acdb4ed86d40bd2852
Nathan Jenan <nathan.jenan@polarity.io>
2018-09-12 06:22:49 -0700

    Moving formatting of results into integration from the view

--- components/vulndb-block.js
+++ components/vulndb-block.js
@@ -1,16 +1,3 @@
-function addItem(items, key, value) {
-    if (value) {
-        items.push({ key: key, value: value, hideable: isHideableKey(key), show: !isHideableKey(key) });
-    }
-}
-
-function isHideableKey(key) {
-    return [
-        'Description',
-        'Solution'
-    ].includes(key);
-}
-
 polarity.export = PolarityComponent.extend({
     init() {
         this._super(...arguments);
@@ -18,47 +5,8 @@ polarity.export = PolarityComponent.extend({
     },
     details: Ember.computed.alias('block.data.details'),
     observer: Ember.on('init', Ember.observer('block.data.details', function () {
-        try {
-            let results = this.get('block.data.details').map(result => {
-                let items = [];
-                addItem(items, 'Description', result.description);
-                addItem(items, 'Discovery Date', result.discovery_date);
-                addItem(items, 'Disclosure Date', result.disclosure_date);
-                addItem(items, 'Exploit Publish Date', result.exploit_publish_date);
-                addItem(items, 'Solution Date', result.solution_date);
-                addItem(items, 'Keywords', result.keywords);
-                addItem(items, 'Solution', result.solution);
-
-                result.products.forEach(product => {
-                    product.versions = product.versions.slice(0, 5);
-                    product.versions.reverse();
-                });
-
-                let authors = result.authors.map(author => {
-                    let items = [];
-                    addItem(items, 'Name', author.name);
-                    addItem(items, 'Company', author.company);
-                    addItem(items, 'Country', author.country);
-                    addItem(items, 'Email', author.email);
-                    return { fields: items };
-                });
-
-                return {
-                    title: result.title,
-                    items: items,
-                    classifications: result.classifications,
-                    authors: authors,
-                    products: result.products.slice(0, 5),
-                    metrics: result.cvss_metrics,
-                    references: result.ext_references,
-                    vtems: result.vtems,
-                    vendors: result.vendors
-                };
-            });
-            this.set('results', results);
-        } catch (e) {
-            console.error(e);
-        }
+        let results = this.get('block.data.details');
+        this.set('results', results);
     })),
     actions: {
         toggle: function (key) {
--- integration.js
+++ integration.js
@@ -88,7 +88,12 @@ function doLookup(entities, options, callback) {
             }
         });
     }, err => {
-        callback(err, results);
+        results.forEach(result => {
+            if (result.data) {
+                result.data.details = result.data.details.map(formatForView);
+            }
+        });
+        callback(err, results)
     });
 }
 
@@ -122,6 +127,57 @@ function startup(logger) {
     requestOptions.json = true;
 
     requestWithDefaults = handleRequestError(request.defaults(requestOptions));
+}
+
+function addItem(items, key, value) {
+    if (value) {
+        items.push({ key: key, value: value, hideable: isHideableKey(key), show: !isHideableKey(key) });
+    }
+}
+
+function isHideableKey(key) {
+    return [
+        'Description',
+        'Solution'
+    ].includes(key);
+}
+
+// Format the response for the view
+function formatForView(result) {
+    let items = [];
+    addItem(items, 'Description', result.description);
+    addItem(items, 'Discovery Date', result.discovery_date);
+    addItem(items, 'Disclosure Date', result.disclosure_date);
+    addItem(items, 'Exploit Publish Date', result.exploit_publish_date);
+    addItem(items, 'Solution Date', result.solution_date);
+    addItem(items, 'Keywords', result.keywords);
+    addItem(items, 'Solution', result.solution);
+
+    result.products.forEach(product => {
+        product.versions = product.versions.slice(0, 5);
+        product.versions.reverse();
+    });
+
+    let authors = result.authors.map(author => {
+        let items = [];
+        addItem(items, 'Name', author.name);
+        addItem(items, 'Company', author.company);
+        addItem(items, 'Country', author.country);
+        addItem(items, 'Email', author.email);
+        return { fields: items };
+    });
+
+    return {
+        title: result.title,
+        items: items,
+        classifications: result.classifications,
+        authors: authors,
+        products: result.products.slice(0, 5),
+        metrics: result.cvss_metrics,
+        references: result.ext_references,
+        vtems: result.vtems,
+        vendors: result.vendors
+    };
 }
 
 function validateStringOption(errors, options, optionName, errMessage) {
